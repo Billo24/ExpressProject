bf96400eaa255f07bd6549f16b3749f48f3e9731
billo24 <billhorrill@gmail.com>
2025-01-05 15:28:12 +0000

    Initial version

---
+++ routes/main.js
@@ -0,0 +1,15 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+// Handle the main routes
+
+router.get("/", (req, res) => res.send("Hello World!")); 
+
+router.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>"));
+router.get("/user/profile", (req, res) => res.send ("<h1>User profile page</h1>"));
+
+
+
+// Export the router object so index.js can access it
+module.exports = router;
