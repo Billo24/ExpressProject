ddd8c4aeb9587e4b176299d6da69fbe7e005d58b
billo24 <billhorrill@gmail.com>
2025-01-05 15:44:49 +0000

    add contact and date

--- routes/main.js
+++ routes/main.js
@@ -8,6 +8,16 @@ router.get("/", (req, res) => res.send("Hello World!"));
 
 router.get("/about", (req, res) => res.send ("<h1>This is the about page</h1>"));
 router.get("/user/profile", (req, res) => res.send ("<h1>User profile page</h1>"));
+router.get("/contact", (req, res) => res.send ("<h1>Contact Information</h1>"));
+router.get("/contact", (req, res) => res.send ("<p1>Contact information goes here</p1>"));
+router.get("/date", (req, res) => res.send ("<h1>Date and time</h1>"));
+
+<script>
+const d = new Date();
+document.getElementById("demo").innerHTML = d;
+</script>
+
+router.get("/date", (req, res) => res.send ("<p id="demo"></p>"));
 
 
 
